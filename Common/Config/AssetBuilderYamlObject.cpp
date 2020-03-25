f78d4e80ec4a567f6278c525a3dd2576ebd75eea
Yukino Hayakawa <tennencoll@gmail.com>
2020-03-25 22:39:22 +0800

    add config module based on yaml

---
+++ Common/Config/AssetBuilderYamlObject.cpp
@@ -0,0 +1,46 @@
+﻿#include "AssetBuilderYamlObject.hpp"
+
+#include <Usagi/Module/Service/Asset/AssetManager.hpp>
+
+namespace usagi
+{
+AssetBuilderYamlObject::AssetBuilderYamlObject(
+    AssetManager *manager,
+    std::u8string locator)
+    : mManager(manager)
+    , mLocator(std::move(locator))
+{
+}
+
+AssetHandle AssetBuilderYamlObject::hash()
+{
+    // todo: use canonical path
+    return crc32c(0, mLocator.data(), mLocator.size());
+}
+
+MemoryRegion AssetBuilderYamlObject::build()
+{
+    auto source = mManager->request_asset(
+        AssetPriority::NORMAL,
+        true,
+        mLocator
+    );
+    source->wait();
+
+    // todo use vm allocator
+    std::unique_ptr<ryml::Tree> tree(new ryml::Tree());
+
+    const auto blob = source->blob();
+    ryml::parse(
+        c4::csubstr { static_cast<char*>(blob.base_address), blob.length },
+        tree.get()
+    );
+
+    return { tree.release(), sizeof(ryml::Tree) };
+}
+
+void AssetBuilderYamlObject::free(const MemoryRegion &mem)
+{
+    delete static_cast<ryml::Tree*>(mem.base_address);
+}
+}
---
+++ Common/Config/AssetBuilderYamlObject.hpp
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+// remove debugbreak dependency
+#pragma push_macro("NDEBUG")
+#define NDEBUG
+#include <ryml.hpp>
+#pragma pop_macro("NDEBUG")
+#pragma comment(lib, "c4core.lib")
+#pragma comment(lib, "ryml.lib")
+
+#include <Usagi/Module/Service/Asset/Asset.hpp>
+
+namespace usagi
+{
+class AssetManager;
+
+class AssetBuilderYamlObject
+{
+    AssetManager *mManager;
+    std::u8string mLocator;
+
+public:
+    AssetBuilderYamlObject(AssetManager *manager, std::u8string locator);
+
+    using OutputT = ryml::Tree;
+    AssetHandle hash();
+    MemoryRegion build();
+    static void free(const MemoryRegion &mem);
+};
+static_assert(AssetBuilder<AssetBuilderYamlObject>);
+}
