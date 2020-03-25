b8b96e23c60b9feb53b20e028c086fa0a23a818c
Yukino Hayakawa <tennencoll@gmail.com>
2020-03-25 22:38:58 +0800

    add transform module

---
+++ Common/Transform/ComponentPosition.hpp
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include <Usagi/Module/Common/Math/Matrix.hpp>
+#include <Usagi/Game/Entity/Component.hpp>
+
+namespace usagi
+{
+class ComponentPosition
+{
+public:
+    Vector3f position = { 0, 0, 0 };
+};
+static_assert(Component<ComponentPosition>);
+}
