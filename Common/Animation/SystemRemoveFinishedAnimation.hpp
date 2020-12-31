a435496f633beced2efeac22e4a042c516560ea8
Yukino Hayakawa <tennencoll@gmail.com>
2020-12-31 19:39:58 +0800

    add animation system

---
+++ Common/Animation/SystemRemoveFinishedAnimation.hpp
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include <Usagi/Entity/EntityDatabase.hpp>
+#include <Usagi/Runtime/Service.hpp>
+
+#include "ComponentAnimator.hpp"
+
+namespace usagi
+{
+struct SystemRemoveFinishedAnimation
+{
+    using WriteAccess = C<ComponentAnimator>;
+    using ReadAccess = C<TagAnimatorLoop>;
+
+    auto update(auto &&rt, auto &&db)
+    {
+        for(auto &&e : db.view(
+            C<ComponentAnimator>(),
+            C<TagAnimatorLoop>()
+        ))
+        {
+            auto &ani = USAGI_COMPONENT(e, ComponentAnimator);
+
+            if(ani.finished())
+                e.remove_component(ComponentAnimator());
+        }
+    }
+};
+}
