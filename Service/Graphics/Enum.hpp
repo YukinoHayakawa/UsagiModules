734eb251e99f72eaebfcce38936bfa20b7723d6e
Yukino Hayakawa <tennencoll@gmail.com>
2021-08-31 19:15:08 +0800

    add graphics service

---
+++ Service/Graphics/Enum.hpp
@@ -0,0 +1,134 @@
+﻿#pragma once
+
+namespace usagi
+{
+// Pipeline
+
+enum class GpuPipelineStage
+{
+    BEFORE_ALL_COMMANDS,
+    VERTEX_INPUT,
+    VERTEX_SHADER,
+    GEOMETRY_SHADER,
+    FRAGMENT_SHADER,
+    EARLY_FRAGMENT_TESTS,
+    LATE_FRAGMENT_TESTS,
+    COLOR_ATTACHMENT_OUTPUT,
+    TRANSFER,
+    TRANSFER_COPY,
+    TRANSFER_RESOLVE,
+    TRANSFER_BLIT,
+    TRANSFER_CLEAR,
+    AFTER_ALL_COMMANDS,
+    HOST,
+};
+
+enum class GpuAccessMask
+{
+    NONE,
+    HOST_READ,
+    HOST_WRITE,
+    TRANSFER_READ,
+    TRANSFER_WRITE,
+    VERTEX_ATTRIBUTE_READ,
+    INDEX_READ,
+    UNIFORM_READ,
+    INPUT_ATTACHMENT_READ,
+    SHADER_READ,
+    SHADER_WRITE,
+    COLOR_ATTACHMENT_READ,
+    COLOR_ATTACHMENT_WRITE,
+    DEPTH_STENCIL_ATTACHMENT_READ,
+    DEPTH_STENCIL_ATTACHMENT_WRITE,
+};
+
+// Images
+
+enum class GpuImageLayout
+{
+    UNDEFINED,
+    PRESENT,
+    TRANSFER_SRC,
+    TRANSFER_DST,
+    ATTACHMENT,
+    COLOR_ATTACHMENT,
+    DEPTH_STENCIL_ATTACHMENT,
+    READ_ONLY,
+    SHADER_READ_ONLY,
+    PREINITIALIZED,
+    AUTO,
+};
+
+enum class GpuImageUsage
+{
+    SAMPLED,
+    COLOR_ATTACHMENT,
+    DEPTH_STENCIL_ATTACHMENT,
+    INPUT_ATTACHMENT,
+};
+
+enum class GpuCompareOp
+{
+    NEVER,
+    LESS,
+    EQUAL,
+    LESS_OR_EQUAL,
+    GREATER,
+    NOT_EQUAL,
+    GREATER_OR_EQUAL,
+    ALWAYS,
+};
+
+// Buffers
+
+enum class GpuBufferFormat
+{
+    R8_UNORM,
+    R8G8_UNORM,
+    R8G8B8_UNORM,
+    R8G8B8A8_UNORM,
+
+    B8G8R8A8_UNORM,
+
+    R32_SFLOAT,
+    R32G32_SFLOAT,
+    R32G32B32_SFLOAT,
+    R32G32B32A32_SFLOAT,
+
+    // Depth stencil formats
+
+    D16_UNORM,
+    D32_SFLOAT,
+    D16_UNORM_S8_UINT,
+    D24_UNORM_S8_UINT,
+    D32_SFLOAT_S8_UINT,
+};
+
+enum class GpuBufferUsage
+{
+    VERTEX,
+    INDEX,
+    UNIFORM,
+};
+
+// Misc
+
+enum class GpuAttachmentLoadOp
+{
+    LOAD,
+    CLEAR,
+    UNDEFINED,
+};
+
+enum class GpuAttachmentStoreOp
+{
+    STORE,
+    UNDEFINED,
+};
+
+enum class GpuIndexType
+{
+    UINT16,
+    UINT32,
+};
+}
