7d1c36635eb128f9f1d265d7925057481998d5f8
Yukino Hayakawa <tennencoll@gmail.com>
2021-10-11 19:02:17 +0800

    move module locations

---
+++ IO/WindowSystemIntegration/SystemSwapchainRelease.hpp
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include <Usagi/Entity/EntityDatabase.hpp>
+#include <Usagi/Modules/Services/Windowing/ComponentNativeWindow.hpp>
+#include <Usagi/Modules/Services/Windowing/ServiceNativeWindowManager.hpp>
+
+// todo remove
+#include <Usagi/Modules/Platforms/Vulkan/VulkanGpuDevice.hpp>
+
+namespace usagi
+{
+USAGI_DECL_ALIAS_SERVICE(
+    ServiceHardwareGraphics,
+    LazyInitService<VulkanGpuDevice>
+);
+
+struct SystemSwapchainRelease
+{
+    using WriteAccess = C<>;
+    using ReadAccess = C<ComponentNativeWindow, TagNativeWindowDestroy>;
+
+    void update(auto &&rt, auto &&db)
+    {
+        for(auto &&e : db.view(ReadAccess()))
+        {
+            auto& wnd_mgr = USAGI_SERVICE(rt, ServiceNativeWindowManager);
+            auto& gfx = USAGI_SERVICE(rt, ServiceHardwareGraphics);
+
+            auto& c_wnd = USAGI_COMPONENT(e, ComponentNativeWindow);
+
+            // if the window is about to be destroyed, destroy the associated
+            // swapchain first
+            NativeWindow* const wnd = wnd_mgr.window(c_wnd.identifier.str());
+            gfx.destroy_swapchain(wnd);
+        }
+    }
+};
+}
