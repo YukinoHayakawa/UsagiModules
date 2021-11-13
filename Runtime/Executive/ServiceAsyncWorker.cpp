13bd53b475ca8550dd06fe9a9c2cae93ab81d814
Yukino Hayakawa <tennencoll@gmail.com>
2021-11-13 16:16:29 +0800

    move std async worked impl to executive (despite it's only a temporary replacement for a more sophisticated impl)

---
+++ Runtime/Executive/ServiceAsyncWorker.cpp
@@ -0,0 +1,55 @@
+﻿#include "ServiceAsyncWorker.hpp"
+
+#include <cassert>
+#include <iostream>
+
+#include <Usagi/Library/Memory/LockGuard.hpp>
+#include <Usagi/Modules/Common/Time/Clock.hpp>
+#include <Usagi/Runtime/Task.hpp>
+
+namespace usagi
+{
+std::uint64_t StdTaskExecutor::submit(
+    std::unique_ptr<Task> task,
+    std::optional<std::vector<std::uint64_t>> wait_on)
+{
+    std::lock_guard lk(mMutex);
+
+    auto task_id = ++mTaskId;
+
+    auto future = std::async(
+        std::launch::async,
+        [t = std::move(task), wait = std::move(wait_on), this]() {
+            Clock clk;
+            // using namespace std::chrono_literals;
+            // std::this_thread::sleep_for(5ms);
+
+            if(wait.has_value())
+            {
+                for(auto &&w : *wait)
+                {
+                    LockGuard lk(mMutex);
+                    auto wt = mTask.find(w);
+                    assert(wt != mTask.end());
+                    lk.unlock();
+                    wt->second.wait();
+                }
+            }
+
+            if(!t->precondition())
+                throw std::runtime_error("");
+            t->on_started();
+            t->run();
+            t->on_finished();
+            if(!t->postcondition())
+                throw std::runtime_error("");
+
+            std::cout << clk.realtime_elapsed() << std::endl;
+        }
+    );
+
+    mTask.emplace(task_id, std::move(future));
+
+    return task_id;
+}
+}
---
+++ Runtime/Executive/ServiceAsyncWorker.hpp
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include <future>
+#include <map>
+
+#include <Usagi/Runtime/TaskExecutor.hpp>
+#include <Usagi/Runtime/Service/SimpleService.hpp>
+
+namespace usagi
+{
+// todo: this is a temporary replacement
+class StdTaskExecutor : public TaskExecutor
+{
+    std::map<std::uint64_t, std::future<void>> mTask;
+    using TaskRef = decltype(mTask)::iterator;
+    std::mutex mMutex;
+    std::uint64_t mTaskId = 0;
+
+public:
+    std::uint64_t submit(
+        std::unique_ptr<Task> task,
+        std::optional<std::vector<std::uint64_t>> wait_on) override;
+};
+
+using ServiceAsyncWorker = SimpleService<StdTaskExecutor>;
+}
