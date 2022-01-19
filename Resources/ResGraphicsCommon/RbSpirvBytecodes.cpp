2911d88e33b064b64c51b8d57d761098f265f2ea
Yukino Hayakawa <tennencoll@gmail.com>
2022-01-19 19:19:12 +0800

    +spirv bytecodes as res

---
+++ Resources/ResGraphicsCommon/RbSpirvBytecodes.cpp
@@ -0,0 +1,40 @@
+﻿#include "RbSpirvBytecodes.hpp"
+
+#include <Usagi/Modules/Runtime/HeapManager/HeapManager.hpp>
+#include <Usagi/Modules/Runtime/Asset/RbAssetMemoryView.hpp>
+#include <Usagi/Modules/Common/Logging/Logging.hpp>
+#include <Usagi/Modules/IO/Graphics/ShaderCompiler.hpp>
+
+namespace usagi
+{
+RbSpirvBytecodes::RbSpirvBytecodes(
+    std::string normalized_asset_path,
+    GpuShaderStage stage)
+    : RbAssetDerivative(std::move(normalized_asset_path))
+    , mStage(stage)
+{
+}
+
+ResourceState RbSpirvBytecodes::construct(
+    ResourceConstructDelegate<RbSpirvBytecodes> &delegate)
+{
+    LOG(info,
+        "[glslang] Compiling {} shader: {}",
+        to_string(mStage),
+        asset_path()
+    );
+
+    const auto shader_source = delegate.resource<
+        RbAssetMemoryView
+    >(asset_path()).await();
+
+    auto bytecodes = spirv::from_glsl(
+        shader_source.to_string_view(),
+        mStage
+    );
+
+    delegate.allocate(std::move(bytecodes));
+
+    return ResourceState::READY;
+}
+}
---
+++ Resources/ResGraphicsCommon/RbSpirvBytecodes.hpp
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include <Usagi/Modules/Runtime/HeapManager/HeapFreeObjectManager.hpp>
+#include <Usagi/Modules/Runtime/HeapManager/ResourceBuilder.hpp>
+#include <Usagi/Modules/Runtime/Asset/RbAssetDerivative.hpp>
+#include <Usagi/Modules/IO/Graphics/Enum.hpp>
+
+#include "SpirvBytecodes.hpp"
+
+namespace usagi
+{
+class RbSpirvBytecodes : RbAssetDerivative
+{
+    GpuShaderStage mStage;
+
+public:
+    RbSpirvBytecodes(std::string normalized_asset_path, GpuShaderStage stage);
+
+    using TargetHeapT = HeapFreeObjectManager;
+    using ProductT = SpirvBytecodes;
+
+    // todo error handling
+    ResourceState construct(
+        ResourceConstructDelegate<RbSpirvBytecodes> &delegate);
+};
+}
