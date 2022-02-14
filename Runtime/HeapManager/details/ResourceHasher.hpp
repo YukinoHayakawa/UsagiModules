908e3ef4d116dc6c31e8157d69f709502367fb5c
Yukino Hayakawa <tennencoll@gmail.com>
2022-02-14 17:55:39 +0800

    Use to_string_view to get the memory view of the parameters being hashed

--- Runtime/HeapManager/details/ResourceHasher.hpp
+++ Runtime/HeapManager/details/ResourceHasher.hpp
@@ -1,6 +1,6 @@
 ﻿#pragma once
 
-#include <Usagi/Concept/Type/Types.hpp>
+#include <Usagi/Library/Utility/StringView.hpp>
 
 #include "../External/xxhash/xxhash64.h"
 
@@ -19,11 +19,9 @@ public:
     std::size_t append(T &&val)
 	{
 		const auto current = mNumProcessedBytes;
-        AppendToHasher<T> appender;
-		appender([&](std::string_view mem_view) {
-			mHasher.add(mem_view.data(), mem_view.size());
-			mNumProcessedBytes += mem_view.size();
-		}, val);
+        auto mem_view = to_string_view(val);
+        mHasher.add(mem_view.data(), mem_view.size());
+        mNumProcessedBytes += mem_view.size();
         return mNumProcessedBytes - current;
 	}
 
@@ -36,42 +34,5 @@ public:
 	{
         return mHasher.hash();
 	}
-};
-
-template <Arithmetic T>
-struct AppendToHasher<T>
-{
-    void operator()(auto &&append_func, const T &val)
-    {
-        std::string_view view {
-            reinterpret_cast<const char *>(&val),
-            sizeof(T)
-        };
-        append_func(view);
-    }
-};
-
-template <StringView T>
-struct AppendToHasher<T>
-{
-    void operator()(auto &&append_func, const T &val)
-    {
-        append_func(std::string_view(val));
-    }
-};
-
-template <Enum T>
-struct AppendToHasher<T>
-{
-    template <typename Func>
-    void operator()(Func &&append_func, const T &val)
-    {
-        using UnderlyingT = std::underlying_type_t<std::remove_cvref_t<T>>;
-        AppendToHasher<UnderlyingT> appender;
-        appender(
-            std::forward<Func>(append_func),
-            static_cast<UnderlyingT>(val)
-        );
-    }
 };
 }
