a6991803db4aa2cff8a697370d04c3ab60c231bb
Yukino Hayakawa <tennencoll@gmail.com>
2022-07-09 20:23:01 +0800

    add webp decoder

---
+++ Resources/ResCommonImages/RbAssetImage.cpp
@@ -0,0 +1,51 @@
+﻿#include "RbAssetImage.hpp"
+
+#include <webp/decode.h>
+
+#include <Usagi/Modules/Resources/ResScratchBuffer/RbScratchBuffer.hpp>
+#include <Usagi/Modules/Runtime/Asset/RbAssetMemoryView.hpp>
+#include <Usagi/Modules/Runtime/HeapManager/HeapManager.hpp>
+
+namespace usagi
+{
+ResourceState RbAssetImage::construct(
+    ResourceConstructDelegate<ProductT> &delegate,
+    const AssetPath &path)
+{
+    // fetch the raw asset. ref count is maintained by the returned pointer.
+    const auto memory = delegate.resource<RbAssetMemoryView>(path).await();
+
+    // validate image format
+    int img_x, img_y;
+    const int suc = WebPGetInfo(
+        memory->as<std::uint8_t>(),
+        memory->size(),
+        &img_x,
+        &img_y
+    );
+    USAGI_ASSERT_THROW(suc, std::runtime_error("Invalid WebP image."));
+
+    const std::size_t num_pixels = img_x * img_y;
+    const std::size_t buf_size = num_pixels * sizeof(ProductT::PixelT);
+
+    // allocate a scratch buffer
+    const auto staging_buf = delegate.resource_transient<
+        RbScratchBuffer<ProductT::PixelT>>(
+        delegate.make_unique_descriptor(),
+        num_pixels
+    ).get();
+
+    const auto out = WebPDecodeRGBAInto(
+        memory->as<std::uint8_t>(),
+        memory->size(),
+        reinterpret_cast<uint8_t *>(staging_buf->get()),
+        buf_size,
+        sizeof(ProductT::PixelT) * img_x
+    );
+    USAGI_ASSERT_THROW(out, std::runtime_error("Failed to decode WebP image."));
+
+    delegate.emplace(*staging_buf, Vector2u32 { img_x, img_y });
+
+    return ResourceState::READY;
+}
+}
---
+++ Resources/ResCommonImages/RbAssetImage.hpp
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <Usagi/Modules/Runtime/HeapManager/ResourceBuilder.hpp>
+#include <Usagi/Modules/Common/Color/Color.hpp>
+#include <Usagi/Modules/Content/Texture/TextureStatic.hpp>
+#include <Usagi/Modules/Runtime/Asset/AssetPath.hpp>
+
+namespace usagi
+{
+class RbAssetImage : public ResourceBuilderDecl<
+    // output 8bit rgba pixels
+    TextureStatic<Color4u8>,
+    const AssetPath &>
+{
+public:
+    ResourceState construct(
+        ResourceConstructDelegate<ProductT> &delegate,
+        const AssetPath &path) override;
+};
+}
