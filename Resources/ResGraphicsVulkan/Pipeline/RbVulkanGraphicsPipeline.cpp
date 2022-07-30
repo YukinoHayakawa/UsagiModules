c3ff5ef36c357640022f23c20a88a677871f038d
Yukino Hayakawa <tennencoll@gmail.com>
2022-07-30 20:02:53 +0800

    extract header

--- Resources/ResGraphicsVulkan/Pipeline/RbVulkanGraphicsPipeline.cpp
+++ Resources/ResGraphicsVulkan/Pipeline/RbVulkanGraphicsPipeline.cpp
@@ -6,53 +6,10 @@
 
 #include <Usagi/Modules/Platforms/Vulkan/VulkanGraphicsPipelineCompiler.hpp>
 #include <Usagi/Modules/Resources/ResJson/RbCascadingJsonConfig.hpp>
+#include <Usagi/Modules/Resources/ResJson/JsonSchemaHelper.hpp>
 #include <Usagi/Modules/Runtime/HeapManager/HeapManager.hpp>
 
 #include "RbVulkanShaderModule.hpp"
-
-// Macros for translating the enums
-
-// ReSharper disable CppClangTidyCppcoreguidelinesMacroUsage
-#define COMMON_HEADER(_key, _dst) \
-    do { \
-        const char *key = _key; \
-        auto &node = config[nlohmann::json::json_pointer(key)]; \
-        [[maybe_unused]] \
-        auto &dst = _dst; \
-/**/
-
-#define FIELD(_key, _dst, _enum_type) \
-    COMMON_HEADER(_key, _dst) \
-        using EnumT = _enum_type;
-#define CHECK_TYPE(_node, _key, _type) \
-    if(!(_node).is_##_type()) \
-        USAGI_THROW(std::runtime_error( \
-            std::format("Unexpected type for key {}: " \
-            "should be a {}, but received a {} instead.", \
-            _key, #_type, (_node).type_name()) \
-        ));
-// Begin with either MATCH_NULL or MATCH, so `value` is defined.
-#define MATCH_NULL(_enum_val) \
-    if(node.is_null()) dst = EnumT::_enum_val; else
-#define MATCH(_str, _enum_val) \
-    if(std::string value = node; value == (_str)) dst = EnumT::_enum_val;
-#define ELSE_(_str, _enum_val) \
-    else if(value == (_str)) dst = EnumT::_enum_val;
-#define END_FIELD() \
-    else \
-        USAGI_THROW(std::runtime_error( \
-            std::format("Invalid value for key {}: {}", key, value) \
-        )); \
-    } while(false); \
-/**/
-
-#define MATCH_BOOL(_key, _dst) \
-    COMMON_HEADER(_key, _dst); \
-        CHECK_TYPE(node, key, boolean) \
-        (_dst) = node.get<bool>(); \
-    } while(false); \
-/**/
-// ReSharper enable CppClangTidyCppcoreguidelinesMacroUsage
 
 namespace usagi
 {
