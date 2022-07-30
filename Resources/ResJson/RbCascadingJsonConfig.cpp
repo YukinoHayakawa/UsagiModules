a1cfbedb1ab373dde1101bc48939e49fac1bd3a5
Yukino Hayakawa <tennencoll@gmail.com>
2022-07-30 20:02:42 +0800

    make json config loader support multiple base documents

--- Resources/ResJson/RbCascadingJsonConfig.cpp
+++ Resources/ResJson/RbCascadingJsonConfig.cpp
@@ -13,14 +13,16 @@ ResourceState RbCascadingJsonConfig::construct(
     ResourceConstructDelegate<ProductT> &delegate,
     const AssetPath &asset_path) 
 {
-    // Fetch the content of the requested config file
+    constexpr auto KEY_INHERIT = "inherit";
+
+    // Fetch the raw content of the requested config file
     const auto doc = delegate.resource<RbJsonDocument>(asset_path).await();
     const auto &cur = *doc;
 
-    // Extract the base config asset path
-    const auto it = cur.find("inherit");
+    // Get the key that contains documents to be included.
+    const auto it = cur.find(KEY_INHERIT);
 
-    // The config doesn't overrides another. We are done here.
+    // The config doesn't overrides another. We are done here. Return as-is.
     if(it == cur.end())
     {
         // copy it
@@ -28,19 +30,66 @@ ResourceState RbCascadingJsonConfig::construct(
         return ResourceState::READY;
     }
 
-    // Otherwise, trace to the parent config it inherits.
-    assert(it->is_string());
-    std::string base_path = *it;
+    // When there is inheritance, the inherit key is checked. If it is an array
+    // of strings, the first document specified in the array will be loaded
+    // and used as the base document. The subsequent documents in the array
+    // will be loaded and applied as patch in the declared order. The content
+    // of the current document will be applied as patch as last and the inherit
+    // key will be removed when the process is finished. If a string is
+    // specified for the inherit key, the process proceeds as if there is only
+    // one string in the array.
 
-    // Recursively request the parent config tree and make a copy of it.
-    auto parent_doc = *delegate.resource<RbCascadingJsonConfig>(
-        base_path
-    ).await();
+    auto &out = delegate.emplace();
 
-    parent_doc.merge_patch(cur);
-    parent_doc.erase("inherit");
+    const auto merge_parent = [&](const std::string &parent_path)
+    {
+        // Request parent document. The described process must have already
+        // been done to it. Request as transient resource since the documents
+        // must be applied sequentially.
+        const auto &parent_doc = 
+            delegate.resource_transient<RbCascadingJsonConfig>(parent_path)
+                .await().cref();
 
-    delegate.emplace(std::move(parent_doc));
+        USAGI_ASSERT_THROW(
+            !parent_doc.contains(KEY_INHERIT), 
+            std::runtime_error(
+                "The inherit key of parent document should have been removed."
+            )
+        );
+
+        out.merge_patch(parent_doc);
+    };
+
+    if(it->is_array())
+    {
+        for(auto &[key, val] : it->items())
+        {
+            USAGI_ASSERT_THROW(
+                val.is_string(),
+                std::runtime_error(
+                    "Path to parent document should be a string."
+                )
+            );
+            merge_parent(val.get_ref<const std::string &>());
+        }
+    }
+    else if(it->is_string())
+    {
+        merge_parent(it->get_ref<const std::string &>());
+    }
+    else
+    {
+        USAGI_THROW(
+            std::runtime_error(
+                "inherit key should be either a string or an array of strings."
+            )
+        );
+    }
+
+    // Apply initial document.
+    out.merge_patch(cur);
+    // Remove the inherit key from the result document.
+    out.erase("inherit");
 
     return ResourceState::READY;
 }
