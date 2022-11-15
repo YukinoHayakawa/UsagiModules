8f2ea9ce0762428d9ef396f880c3cc76568ef9ec
Yukino Hayakawa <tennencoll@gmail.com>
2022-11-15 19:58:49 +0800

    add SystemVisitEntities and EntityVisitorTransformSingleComponent

---
+++ Common/Functional/Visitors/SystemVisitEntities.hpp
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include <Usagi/Entity/EntityDatabase.hpp>
+#include <Usagi/Runtime/Service/ServiceAccess.hpp>
+
+namespace usagi
+{
+/**
+ * \brief Visit entities that satisfies the provided query.
+ * The query filter defined by it is also applied.
+ * \tparam Query The query specifying the entity query filter.
+ * \tparam Visitor The callable that will be applied to visited entities.
+ * It must accept the entity view and the sorted key defined by the index
+ * descriptor as arguments.
+ *
+ * todo: thread safety
+ */
+template <
+    SimpleComponentQuery Query,
+    typename Visitor
+>
+struct SystemVisitEntities
+{
+    using WriteAccess = typename Visitor::WriteAccess;
+    using ReadAccess = typename Query::ReadAccess;
+
+    // declare access to entity index
+    using ServiceAccessT = ServiceAccess<>;
+
+    void update(ServiceAccessT rt, auto &&db)
+    {
+        Visitor visitor;
+
+        // todo: multithreading
+        for(auto &&entity_view : db.view(Query()))
+        {
+            visitor(entity_view);
+        }
+    }
+};
+}
---
+++ Common/Functional/Visitors/Transformative.hpp
@@ -0,0 +1,54 @@
+﻿#pragma once
+
+#include <iterator>
+
+#include <Usagi/Entity/Component.hpp>
+#include <Usagi/Entity/detail/ComponentFilter.hpp>
+
+namespace usagi
+{
+/**
+ * \brief Transform one component to another by applying a binary operation.
+ * \tparam BinaryTransformOp The transform operator.
+ * \tparam SourceComponent The component to be read from.
+ * \tparam TargetComponent The component to be written to.
+ * \tparam SourceProjection Projection applied to source.
+ * \tparam TargetProjection Projection applied to target.
+ */
+template <
+    Component SourceComponent,
+    Component TargetComponent,
+    typename BinaryTransformOp
+    // typename SourceProjection = std::identity,
+    // typename TargetProjection = std::identity
+> requires
+    // transform op is able to read from source and write to target.
+    std::is_invocable_v<
+        BinaryTransformOp,
+        const SourceComponent &,
+        TargetComponent &
+    >
+    // transform op is able to read from projected source and write to projected
+    // target.
+    // std::is_invocable_v<
+    //     BinaryTransformOp,
+    //     const std::invoke_result_t<SourceProjection, const SourceComponent &> &,
+    //     std::invoke_result_t<TargetProjection, TargetComponent &> &
+    // >
+struct EntityVisitorTransformSingleComponent
+{
+    using WriteAccess = C<TargetComponent>;
+
+    BinaryTransformOp op;
+    // SourceProjection src_proj;
+    // TargetProjection dst_proj;
+
+    void operator()(auto &&entity_view)
+    {
+        const auto &src_comp = entity_view(C<SourceComponent>());
+        auto &dst_comp = entity_view.add_component(C<TargetComponent>());
+        // op(std::invoke(src_proj, src_comp), std::invoke(dst_proj, dst_comp));
+        op(src_comp, dst_comp);
+    }
+};
+}
