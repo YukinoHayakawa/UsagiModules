4eabeaee03f5a8444bd4c9964f9ff641d59269fc
Yukino Hayakawa <tennencoll@gmail.com>
2022-11-18 17:37:54 +0800

    add thread local rng service

---
+++ Algorithms/Statistics/RandomNumbers/ServiceRandomNumberGenerator.hpp
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include <random>
+
+#include <Usagi/Runtime/Service/ServiceAccess.hpp>
+
+namespace usagi
+{
+struct ServiceThreadLocalRNG
+{
+    using ServiceT = std::mt19937;
+
+    static ServiceT & get_service()
+    {
+        // todo make it determinative?
+        thread_local static std::mt19937 rng { std::random_device{}() };
+        return rng;
+    }
+};
+}
+USAGI_DECL_SERVICE_ALIAS(usagi::ServiceThreadLocalRNG, thread_rng);
