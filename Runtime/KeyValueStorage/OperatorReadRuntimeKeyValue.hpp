9bedf5ecc5230f1d64ea891e0a275ff84d3330e1
Yukino Hayakawa <tennencoll@gmail.com>
2023-01-02 20:12:05 +0800

    add runtime key value storage service

---
+++ Runtime/KeyValueStorage/OperatorReadRuntimeKeyValue.hpp
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include <fixed_string.hpp>
+
+#include <Usagi/Runtime/Service/ServiceAccess.hpp>
+
+#include "ServiceRuntimeKeyValueStorage.hpp"
+
+namespace usagi
+{
+template <fixstr::fixed_string Key, typename Value>
+struct OperatorReadRuntimeKeyValue
+{
+    using ServiceAccessT = ServiceAccess<ServiceRuntimeKeyValueStorage>;
+
+    Value operator()(ServiceAccessT &rt, auto &&db)
+    {
+        return rt.kv_storage().require<Value>(Key);
+    }
+};
+}
---
+++ Runtime/KeyValueStorage/ServiceRuntimeKeyValueStorage.hpp
@@ -0,0 +1,61 @@
+﻿#pragma once
+
+#include <map>
+#include <string>
+#include <variant>
+#include <Usagi/Entity/detail/EntityId.hpp>
+#include <Usagi/Runtime/ErrorHandling.hpp>
+#include <Usagi/Runtime/Service/ServiceAccess.hpp>
+#include <Usagi/Runtime/Service/SimpleService.hpp>
+
+namespace usagi
+{
+// todo thread safety
+struct RuntimeKeyValueStorage
+{
+    std::map<std::string, std::variant<
+        std::string,
+        EntityId,
+        double,
+        float,
+        std::uint64_t,
+        std::int64_t,
+        std::int32_t,
+        bool
+    >, std::less<>> values;
+
+    template <typename Val>
+    Val & require(const std::string_view key)
+    {
+        const auto iter = values.find(key);
+        USAGI_ASSERT_THROW(
+            iter != values.end(),
+            std::runtime_error("key not found")
+        );
+        // may throw std::bad_variant_access
+        return std::get<Val>(iter->second);
+    }
+
+    template <typename Val>
+    Val & ensure(const std::string_view key)
+    {
+        auto iter = values.find(key);
+        if(iter != values.end())
+        {
+            USAGI_ASSERT_THROW(
+                std::holds_alternative<Val>(iter->second),
+                std::runtime_error("unmatched value type")
+            );
+        }
+        else
+        {
+            iter = values.emplace(key, Val { }).first;
+        }
+        return std::get<Val>(iter->second);
+    }
+};
+
+using ServiceRuntimeKeyValueStorage = SimpleService<RuntimeKeyValueStorage>;
+}
+
+USAGI_DECL_SERVICE_ALIAS(usagi::ServiceRuntimeKeyValueStorage, kv_storage);
