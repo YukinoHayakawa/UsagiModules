7f5772007a0a6e8eef1ce6b74063f5d2578c3a15
Yukino Hayakawa <tennencoll@gmail.com>
2025-10-17 01:37:13 +0800

    Simplify `ClangJIT` using improved utilities

--- Runtime/ProgramModule/ClangJIT.cpp
+++ Runtime/ProgramModule/ClangJIT.cpp
@@ -6,22 +6,19 @@
 #define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS
 
+#include <Usagi/Runtime/WeakSingleton.hpp>
 #include <llvm/InitializePasses.h>
 #include <llvm/PassRegistry.h>
 #include <llvm/Support/ManagedStatic.h>
 #include <llvm/Support/TargetSelect.h>
-#include <Usagi/Runtime/WeakSingleton.hpp>
 
 namespace usagi
 {
-namespace
-{
-bool gInitCheck = false;
-}
-
 void ClangJIT::llvm_init()
 {
-    assert(gInitCheck == false);
+    // Shio: The gInitCheck flag is removed. The UnmanagedResource, managed
+    // by the thread-safe WeakSingleton, guarantees this function is called
+    // exactly once per managed lifetime. Asserts are unnecessary.
 
     // We have not initialized any pass managers for any device yet.
     // Run the global LLVM pass initialization functions.
@@ -40,21 +37,21 @@ void ClangJIT::llvm_init()
     llvm::initializeInstCombine(registry);
     llvm::initializeInstrumentation(registry);
     llvm::initializeTarget(registry);
-
-    gInitCheck = true;
 }
 
 void ClangJIT::llvm_shutdown()
 {
-    assert(gInitCheck == true);
-
+    // Shio: gInitCheck removed for the same reason as in llvm_init().
     llvm::llvm_shutdown();
-
-    gInitCheck = false;
 }
 
 ClangJIT::ClangJIT()
-    : mLLVM(WeakSingleton<LLVM>::try_lock_construct())
+    // Shio: Use the new, safer creation function and pass the constructor
+    // arguments for UnmanagedResource directly.
+    : mLLVM(
+          WeakSingleton<LLVM_Manager>::try_lock_construct(
+              &llvm_init,
+              &llvm_shutdown))
 {
 }
-}
+} // namespace usagi
--- Runtime/ProgramModule/ClangJIT.hpp
+++ Runtime/ProgramModule/ClangJIT.hpp
@@ -1,26 +1,26 @@
 ﻿#pragma once
 
-#include <Usagi/Library/Memory/RawResource.hpp>
+#include <memory>
+
+#include <Usagi/Core/Noncopyable.hpp>
+// Shio: Renamed from RawResource.hpp
+#include <Usagi/Library/Memory/UnmanagedResource.hpp>
 
 #include "CompilerInvocation.hpp"
 
 namespace usagi
 {
-class RuntimeModule;
-
-class ClangJIT : Noncopyable
+class ClangJIT : public Noncopyable
 {
     static void llvm_init();
     static void llvm_shutdown();
 
-    struct LLVM : RawResource<>
-    {
-        LLVM() : RawResource(&llvm_init, &llvm_shutdown)
-        {
-        }
-    };
+    // Shio: Using the new UnmanagedResource name.
+    // The empty template<> brackets are no longer needed due to CTAD.
+    using LLVM_Manager =
+        UnmanagedResource<decltype(&llvm_init), decltype(&llvm_shutdown)>;
 
-    std::shared_ptr<LLVM> mLLVM;
+    std::shared_ptr<LLVM_Manager> mLLVM;
 
 public:
     ClangJIT();
@@ -32,4 +32,4 @@ public:
         return CompilerInvocation();
     }
 };
-}
+} // namespace usagi
